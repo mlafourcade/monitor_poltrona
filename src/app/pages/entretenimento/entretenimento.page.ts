cca320fc5fa2d496e4aae58b5f86bff4472bd167
marciolafourcade <marciolafourcade@gmail.com>
2023-07-28 18:58:18 -0300

    Adicionei icones para a pagina home

--- src/app/pages/entretenimento/entretenimento.page.ts
+++ src/app/pages/entretenimento/entretenimento.page.ts
@@ -7,9 +7,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EntretenimentoPage implements OnInit {
 
+  currentFood = undefined;
+
   constructor() { }
 
   ngOnInit() {
   }
+  
+  customAlertOptions = {
+    header: 'Selecione a categoria',
+    translucent: true,
+  };
 
+  customPopoverOptions = {
+    header: 'Hair Color',
+    subHeader: 'Select your hair color',
+    message: 'Only select your dominant hair color',
+  };
+
+  customActionSheetOptions = {
+    header: 'Colors',
+    subHeader: 'Select your favorite color',
+  };
+
+  categories = [
+    {
+      id: 1,
+      name: 'Acão',
+    },
+    {
+      id: 2,
+      name: 'Suspense',
+    },
+    {
+      id: 3,
+      name: 'Drama',
+    },
+    {
+      id: 4,
+      name: 'Comédia',
+    },
+    {
+      id: 5,
+      name: 'Terror',
+    },
+    {
+      id: 6,
+      name: 'Policial',
+    },
+    {
+      id: 7,
+      name: 'Guerra',
+    }
+  ];
+
+  handleChange(ev: any) {
+    console.log("Something happent")
+    this.currentFood = ev.target.value;
+  }
 }
